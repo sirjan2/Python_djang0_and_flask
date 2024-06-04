f50ce428a5d6c31478ec5f18af239d4db452600c
sirjan2 <sirjanshrestha690@gmail.com>
2024-06-04 17:38:33 +0545

    URL_parameter

--- project/home/urls.py
+++ project/home/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name="home"),
-    path('products/',views.products,name="products")
+    # path('',views.home,name="home"),
+    # path('products/',views.products,name="products")
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]

3e4a5c72ef822f3cbe756f3a22f4b88695d761e7
sirjan2 <sirjanshrestha690@gmail.com>
2024-06-08 22:08:46 +0545

    Customizing admin

--- project/home/admin.py
+++ project/home/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from . import models
+class ChoiceInline(admin.StackedInline):
+    model = models.Choice
+    extra = 2
 # Register your models here.
-admin.site.register(models.Question)
-admin.site.register(models.Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    fields=['pub_date','question_text']
+    inlines=[ChoiceInline]
+    list_display=['pub_date','question_text']
+    list_editable=['question_text']
+    list_filter=['pub_date']
+# Changing admin fields.
+class ChoiceAdmin(admin.ModelAdmin):
+    fields=['votes','question','choice_text']
+admin.site.register(models.Question,QuestionAdmin)
+admin.site.register(models.Choice,ChoiceAdmin)
